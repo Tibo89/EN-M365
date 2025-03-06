82aabfd22deb1496dd4381509c6377f65fa34ee1
Thibaut LE CHAMPION <105300149+Tibo89@users.noreply.github.com>
2025-03-06 12:05:47 +0100

    First push

--- ExtractAuthMethods_SMS_VC_Email/ExtractAuthMethods_SMS_VC_Email.ps1
+++ ExtractAuthMethods_SMS_VC_Email/ExtractAuthMethods_SMS_VC_Email.ps1
@@ -1,1 +1,67 @@
+Clear-Host
 
+$countSMS = 0
+$countPhone = 0
+$countMail = 0
+$countTotal = 0
+$countOnly = 0
+
+$myDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+[xml]$ConfigFile = Get-Content ($MyDir+"\Params.xml")
+
+
+$pathSMSFile = ($myDir+"\"+$ConfigFile.Settings.Paths.SMSFile)
+$pathPhoneFile = ($myDir+"\"+$ConfigFile.Settings.Paths.PhoneFile)
+$pathEmailFile = ($myDir+"\"+$ConfigFile.Settings.Paths.EmailFile)
+$pathDeprecatedMethodsFile = ($myDir+"\"+$ConfigFile.Settings.Paths.DeprecatedMethodsFile)
+
+Set-Content -Path $pathSMSFile -Value "UPN;PhoneNumber"
+Set-Content -Path $pathPhoneFile -Value "UPN;PhoneNumber"
+Set-Content -Path $pathEmailFile -Value "UPN;mail"
+Set-Content -Path $pathDeprecatedMethodsFile -Value "UPN"
+
+Get-MgUser -All | ForEach-Object{
+    $upn = $_.UserPrincipalName
+    $id = $_.Id
+    $countTotal++
+    $userCount = 1
+    try{
+        $authMethods = Get-MgUserAuthenticationMethod -UserId $id -ErrorAction Stop
+        $authMethods | ForEach-Object{
+            
+            if($_.AdditionalProperties['@odata.type'] -like "*phone*" -and $_.AdditionalProperties['smsSignInState'] -eq "ready"){
+                Write-Host ("$upn with SMS method, phone number : " + $_.AdditionalProperties['phoneNumber'])
+                $countSMS++
+                $userCount++
+                Add-Content -Path $pathSMSFile -Value ($upn+";"+$_.AdditionalProperties['phoneNumber'])
+            }
+            elseif($_.AdditionalProperties['@odata.type'] -like "*phone*" -and $_.AdditionalProperties['smsSignInState'] -eq "notSupported"){
+                Write-Host ("$upn with Voice Call method, phone number : " + $_.AdditionalProperties['phoneNumber'])
+                $countPhone++
+                $userCount++
+                Add-Content -Path $pathPhoneFile -Value ($upn+";"+$_.AdditionalProperties['phoneNumber'])
+            }
+            elseif($_.AdditionalProperties['@odata.type'] -like "*email*"){
+                Write-Host ("$upn with OTP Mail, address : " + $_.AdditionalProperties['emailAddress'])
+                $countMail++
+                $userCount++
+                Add-Content -Path $pathEmailFile -Value ($upn+";"+$_.AdditionalProperties['emailAddress']) 
+            }
+        }
+        if($userCount -eq ($authMethods.count) -and ($authMethods.count) -gt 1){
+            Write-Host ("$upn has registred only deprecated method(s)") -ForegroundColor Red
+            $countOnly++
+            Add-Content -Path $pathDeprecatedMethodsFile -Value ($upn)
+        }
+    }
+    catch{
+        Write-Host ("[ERROR] UPN $upn") -ForegroundColor Red
+        Write-Host ("[ERROR]  "+ $_.Exception.Message) -ForegroundColor Red
+    }
+}
+
+Write-Host ("")
+Write-Host ("$countSMS users out of $countTotal with SMS Signin method")
+Write-Host ("$countPhone users out of $countTotal with Voice Call method")
+Write-Host ("$countMail users out of $countTotal with Mail method")
+Write-Host ("$countOnly users out of $countTotal with only SMS and/or Voice Call and/or Mail OTP method") -ForegroundColor Red
---
+++ ExtractAuthMethods_SMS_VC_Email/Readme.md
@@ -0,0 +1,8 @@
+## Description
+
+Extract all Entra ID users and checks if they have SMS/Voice Call/Email OTP authentication method regsitred. The results are stored in the files mentionned in the params.xml file
+
+## Prerequisites
+
+- The MgGraph module must be installed
+- API authorization must be configured for the execution account, you can update the script to call App Registration
